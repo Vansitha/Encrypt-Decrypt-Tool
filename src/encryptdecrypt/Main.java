da78227963f4afdfff6ef3716bbb75950737d9da
Vansitha <vansitha1@gmail.com>
2022-06-15 17:48:51 +0530

    Initial state of the program

---
+++ src/encryptdecrypt/Main.java
@@ -0,0 +1,16 @@
+package encryptdecrypt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "we found a treasure!";
+
+    }
+
+    public static String encrptyMessaage(String message)
+    {
+        for (int i = 0; i < message.length(); i++) {
+            if(message.charAt(i) ==)
+        }
+    }
+}
