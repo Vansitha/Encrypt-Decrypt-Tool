aa95e5d6b23df07c41fe0eebd994fcd48fdd23f9
Vansitha <vansitha1@gmail.com>
2022-06-18 10:54:40 +0530

    "Completed method to replace each letter with a letter from the end of the alphabet"

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -2,15 +2,43 @@ package encryptdecrypt;
 
 public class Main {
 
+    private static final char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
     public static void main(String[] args) {
         String message = "we found a treasure!";
-
+        String newMessage = encryptMessage(message);
+        System.out.println(newMessage);
     }
 
-    public static String encrptyMessaage(String message)
+    /**
+     * Creates a new string by replacing each letter in the provided string
+     * with the letter that is in the corresponding position from the end of the
+     * alphabet.
+     *
+     * @param message string containing a message to be encrypted
+     * @return new shuffled string
+     */
+    public static String encryptMessage(String message)
     {
-        for (int i = 0; i < message.length(); i++) {
-            if(message.charAt(i) ==)
+        int totalIndices = alphabet.length - 1;
+        StringBuilder encryptedStr = new StringBuilder();
+
+        // loop through the message string
+        for(int i = 0; i < message.length(); i++){
+            if (Character.isLetter(message.charAt(i))) {
+                int indexInAlphaArray = 0;
+
+                // loop through the alphabet array
+                for (int j = 0; j < alphabet.length; j++) {
+                    if (message.charAt(i) == alphabet[j])
+                        indexInAlphaArray = j;
+                }
+                // find and replace letter from the end of the alphabet array
+                encryptedStr.append(alphabet[totalIndices - indexInAlphaArray]);
+            } else {
+                encryptedStr.append(message.charAt(i));
+            }
         }
+        return encryptedStr.toString();
     }
 }
